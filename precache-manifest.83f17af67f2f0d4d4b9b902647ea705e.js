ceda596d5c706b6fe02829ea91e6a42868a6a48b
Benjamin Brier <ben.brier14@gmail.com>
2020-02-18 17:19:29 -0500

    Updates

---
+++ precache-manifest.83f17af67f2f0d4d4b9b902647ea705e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c65ed709b3febcbdce3668f7296d55",
+    "url": "/RecipeApp/index.html"
+  },
+  {
+    "revision": "5e1f4100456abf674036",
+    "url": "/RecipeApp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1f33aaa192bbd90355df",
+    "url": "/RecipeApp/static/js/2.b3611cfd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/RecipeApp/static/js/2.b3611cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e1f4100456abf674036",
+    "url": "/RecipeApp/static/js/main.1a2a9f34.chunk.js"
+  },
+  {
+    "revision": "8a995e638993516eb11f",
+    "url": "/RecipeApp/static/js/runtime-main.1abaac6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RecipeApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
