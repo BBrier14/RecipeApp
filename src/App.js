552dae7253e232a24879c245a8440a2c76632aed
Benjamin Brier <ben.brier14@gmail.com>
2020-02-18 20:33:54 -0500

    Recipe Component scaffolding works

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import Recipe from './Recipe';
 import './App.css';
 
 const App = () => {
 	const APP_ID = '08056da4';
 	const APP_KEY = '4cf4ce1575c6f9fa4526464f36e28633';
 
-	useEffect(() => {}, []);
+	const [ recipes, setRecipes ] = useState([]);
+
+	useEffect(() => {
+		getRecipes();
+	}, []);
 
 	const getRecipes = async () => {
 		const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
 		const data = await response.json();
-		console.log(data);
+		setRecipes(data.hits);
 	};
 
 	return (
@@ -22,6 +26,7 @@ const App = () => {
 					Search
 				</button>
 			</form>
+			{recipes.map((recipe) => <Recipe />)};
 		</div>
 	);
 };
