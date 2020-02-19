831dde0eb9cf13297551caa5ba7eb87cd825055c
Benjamin Brier <ben.brier14@gmail.com>
2020-02-18 20:17:50 -0500

    getrecipe function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,12 +6,22 @@ const App = () => {
 	const APP_ID = '08056da4';
 	const APP_KEY = '4cf4ce1575c6f9fa4526464f36e28633';
 
-	const exampleReq =
-		`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+	useEffect(() => {}, []);
+
+	const getRecipes = async () => {
+		const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+		const data = await response.json();
+		console.log(data);
+	};
 
 	return (
 		<div className="App">
-			
+			<form className="search-form">
+				<input className="search-bar" type="text" />
+				<button className="search-button" type="submit">
+					Search
+				</button>
+			</form>
 		</div>
 	);
 };
